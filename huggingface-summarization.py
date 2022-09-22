cd27d1368046c6f17386e43f8a0cae153577505e
Matthew <80968027+MLaunch@users.noreply.github.com>
2022-09-22 13:41:43 -0400

    Update huggingface-summarization.py

--- huggingface-summarization.py
+++ huggingface-summarization.py
@@ -37,8 +37,7 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'headers': {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': true
+            'Access-Control-Allow-Origin': '*'
         },
         'body': sResult #return answers to caller
     }
