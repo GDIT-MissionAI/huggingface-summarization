db7a375e3e74c421df8f5b3a14a99c6665116228
Matthew <80968027+MLaunch@users.noreply.github.com>
2022-11-16 17:35:52 -0500

    Update huggingface-summarization.py

--- huggingface-summarization.py
+++ huggingface-summarization.py
@@ -10,7 +10,7 @@ from base64 import b64encode
 #load pipeline
 #summarizer = pipeline("summarization")
 #summarizer = pipeline(model = "sshleifer/distilbart-cnn-12-6", tokenizer ="sshleifer/distilbart-cnn-12-6")
-summarizer = pipeline(model = "models/distilbart-cnn-12-6", tokenizer ="models/distilbart-cnn-12-6")
+summarizer = pipeline(task = "summarization", model = "models/distilbart-cnn-12-6", tokenizer ="models/distilbart-cnn-12-6")
 
 
 #load clients
